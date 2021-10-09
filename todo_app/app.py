7b0f3a64270101b1efc4b862644585a5873a2099
Ross Greene <ross.greene@softwire.com>
2021-10-09 15:06:47 +0100

    Ex1 display list, add items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
+from todo_app.data.session_items import get_items, add_item
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config())
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return 'Hello World!'
+    return render_template("index.html", items=get_items())
+
+
+@app.route("/add-item", methods=["POST"])
+def add():
+    new_item = request.form.get("new_item")
+    add_item(new_item)
+    return render_template("index.html", items=get_items())
