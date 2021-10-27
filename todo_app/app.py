a6a53bdbfbc93103906d6e898a32800cdd1618ab
Ross Greene <ross.greene@softwire.com>
2021-10-27 16:14:32 +0100

    M1: Complete & delete items from app

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
-from todo_app.data.session_items import get_items, add_item
+from todo_app.data.session_items import add_item, get_item, remove_item, get_sorted_items
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
@@ -9,11 +9,24 @@ app.config.from_object(Config())
 
 @app.route("/")
 def index():
-    return render_template("index.html", items=get_items())
+    return render_template("index.html", items=get_sorted_items())
 
 
-@app.route("/add-item", methods=["POST"])
+@app.route("/add", methods=["POST"])
 def add():
     new_item = request.form.get("new_item")
     add_item(new_item)
-    return render_template("index.html", items=get_items())
+    return redirect("/")
+
+
+@app.route("/complete", methods=["POST"])
+def complete():
+    item_to_complete = get_item(request.form.get("id_to_complete"))
+    item_to_complete["status"] = "Completed"
+    return redirect("/")
+
+
+@app.route("/delete", methods=["POST"])
+def remove():
+    remove_item(int(request.form.get("id_to_delete")))
+    return redirect("/")
--- todo_app/data/session_items.py
+++ todo_app/data/session_items.py
@@ -7,45 +7,24 @@ _DEFAULT_ITEMS = [
 
 
 def get_items():
-    """
-    Fetches all saved items from the session.
-
-    Returns:
-        list: The list of saved items.
-    """
     return session.get("items", _DEFAULT_ITEMS.copy())
 
 
+def get_sorted_items():
+    return sorted(get_items(), key=lambda item: item["status"] == "Completed")
+
+
 def get_item(id):
-    """
-    Fetches the saved item with the specified ID.
-
-    Args:
-        id: The ID of the item.
-
-    Returns:
-        item: The saved item, or None if no items match the specified ID.
-    """
     items = get_items()
     return next((item for item in items if item["id"] == int(id)), None)
 
 
 def add_item(title):
-    """
-    Adds a new item with the specified title to the session.
-
-    Args:
-        title: The title of the item.
-
-    Returns:
-        item: The saved item.
-    """
     items = get_items()
 
     # Determine the ID for the item based on that of the previously added item
-    id = items[-1]["id"] + 1 if items else 0
-
-    item = {"id": id, "title": title, "status": "Not Started"}
+    id_ = items[-1]["id"] + 1 if items else 0
+    item = {"id": id_, "title": title, "status": "Not Started"}
 
     # Add the item to the list
     items.append(item)
@@ -57,9 +36,6 @@ def add_item(title):
 def save_item(item):
     """
     Updates an existing item in the session. If no existing item matches the ID of the specified item, nothing is saved.
-
-    Args:
-        item: The item to save.
     """
     existing_items = get_items()
     updated_items = [
@@ -70,3 +46,10 @@ def save_item(item):
     session["items"] = updated_items
 
     return item
+
+
+def remove_item(item_id):
+    print([item["id"] for item in session["items"]])
+    print(item_id)
+
+    session["items"] = [item for item in session["items"] if item["id"] != item_id]
